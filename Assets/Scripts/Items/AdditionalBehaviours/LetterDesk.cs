3d0b78638b90caa8612935ab45a999a4a1c7c0fd
Vanessa <vanessa.dietl@students.fhv.at>
2024-11-16 22:22:50 +0100

    made a script

---
+++ Assets/Scripts/Items/AdditionalBehaviours/LetterDesk.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterDesk : MonoBehaviour
+{
+
+    [SerializeField] Inventory inventory;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
