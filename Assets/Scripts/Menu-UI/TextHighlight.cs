c464d657c7f1de12c28b148961fb50aee6e92634
Vanessa <vanessa.dietl@students.fhv.at>
2024-11-16 19:15:58 +0100

    fixed main menu

--- Assets/Scripts/Menu-UI/TextHighlight.cs
+++ Assets/Scripts/Menu-UI/TextHighlight.cs
@@ -1,18 +1,29 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class TextHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] Image image;
     [SerializeField] TextMeshProUGUI text;
     private Color normalColor;
     private Color hoverColor;
 
+    private Color normalTextColor;
+    private Color hoverTextColor;
+
     void Start()
     {
 
+    }
+
+    void Update()
+    {
 
     }
 
@@ -20,13 +31,38 @@ public class TextHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public void OnPointerEnter(PointerEventData eventData) 
     {
 
-        ColorUtility.TryParseHtmlString("#FE0404", out Color hoverColor);
-        text.color = hoverColor;
+        ColorUtility.TryParseHtmlString("#FF0021", out Color hoverColor);
+        ColorUtility.TryParseHtmlString("#FF0000", out Color hoverTextColor);
+
+        if(image != null)
+        {
+
+            image.color = hoverColor;
+
+        } else
+        {
+            text.color = hoverTextColor;
+
+        }
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        ColorUtility.TryParseHtmlString("#AB0000", out Color normalColor);
-        text.color = normalColor;
+        ColorUtility.TryParseHtmlString("#FFFFFF", out Color normalColor);
+        ColorUtility.TryParseHtmlString("#830000", out Color normalTextColor);
+
+        if(image != null)
+        {
+
+            image.color = normalColor;
+
+        } else
+        {
+
+            text.color = normalTextColor;
+
+        }
+
     }
 }
