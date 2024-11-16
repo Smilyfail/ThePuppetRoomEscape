a62e40a1124e11cf5c2f267f1ea5deb208296de1
Smilyfail <dario.birbamer@gmail.com>
2024-11-16 14:33:38 +0100

    Added inventory add, 1 inventory slot

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     List<string> itemsInInventory;
+    [SerializeField] List<GameObject> inventorySlots;
+    private bool[] isSlotOccupied;
+    [SerializeField] List<Sprite> itemSprites;
 
     private void Start()
     {
@@ -15,14 +17,21 @@ public class Inventory : MonoBehaviour
     public void AddItemToInventory(string itemName)
     {
         itemsInInventory.Add(itemName);
-        
-        foreach (string item in itemsInInventory) 
+
+        foreach (string item in itemsInInventory)
             Debug.Log(item);
+
+        var itemPosition = itemsInInventory.Count - 1;
+
+        inventorySlots[itemPosition].GetComponent<Image>().sprite = itemSprites.Find(x => x.name == itemName);
+        inventorySlots[itemPosition].gameObject.SetActive(true);
     }
 
     public void RemoveItemFromInventory(string itemName) 
     {
-        if (itemsInInventory.Contains(itemName)) 
-            itemsInInventory.Remove(itemName);
+        if (itemsInInventory.Contains(itemName))
+        {
+
+        }
     }
 }
